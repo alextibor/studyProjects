9d52f7b1b6a1f65e752f9b0cdd286c2610366bcc
alextibor <alex.tibor@hotmail.com>
2021-09-20 03:20:29 -0300

    implementing the product logic

--- libraryCSharp/Model/Product.cs
+++ libraryCSharp/Model/Product.cs
@@ -1,7 +1,21 @@
 namespace libraryCSharp.Model
 {
-    public class Product
+    public abstract class Product
     {
+        protected string productName { get; set; }
+        protected string description { get; set; }
+        protected double value { get; set; }
+        protected string isbn { get; set; }
+        protected Author author { get; set; }
+        protected PublishingCompany pc { get; set; }
+        protected bool printed { get; set; }
         
+        public void setAuthorName(string authorName){
+            author.set(authorName);
+        }
+
+        public void setPublishingCompanyName(string pcName){
+            pc.setPcName(pcName);
+        }
     }
 }
