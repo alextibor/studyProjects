c159e5ca10a2b58319faf773c55d90f0a57073fd
AlexPcTrampo <alex.tibor@hotmail.com>
2021-09-24 19:25:00 -0300

    part4

---
+++ Part4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+using ByteBank;
+
+namespace AluraCSharpOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(7480, 874520);
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
