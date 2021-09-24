7d2391512aae024c30d13d3ccabe52b6e5307916
alextibor <alex.tibor@hotmail.com>
2021-09-24 02:26:28 -0300

    start Exceptions

---
+++ AluraCSharp/EntendendoExcecoes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EntendendoExcecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ AluraCSharp/PT4EntendendoExcecoes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PT4EntendendoExcecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
