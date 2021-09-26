0f66e590059debd6a73f2545fb9bcb9b35b729f9
alextibor <alex.tibor@hotmail.com>
2021-09-26 03:06:55 -0300

    create Position class

---
+++ CSharpChess/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CSharpChess.Board;
+
+namespace CSharpChess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Position P = new Position(3, 4);
+
+            Console.WriteLine("Position: " + P);
+
+            Console.ReadKey();
+        }
+    }
+}
