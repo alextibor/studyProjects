771204566f2fc951ad54169c1ecfebb6fd92c6d1
AlexPcTrampo <alex.tibor@hotmail.com>
2021-09-27 19:22:04 -0300

    Implement class ChessPosition

--- CSharpChess/Program.cs
+++ CSharpChess/Program.cs
@@ -8,19 +8,12 @@ namespace CSharpChess
     {
         static void Main(string[] args)
         {
-            Board bd = new Board(8, 8);
+            ChessPosition pos = new ChessPosition('c', 7);
+            
+            Console.WriteLine(pos);
 
-            try
-            {
-                bd.putPiece(new Tower(bd, Color.Black), new Position(0, 9));
-                bd.putPiece(new Tower(bd, Color.Black), new Position(1, 9));
-                bd.putPiece(new King(bd, Color.Black), new Position(0, 2));
-            }
-            catch (BoardException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            Console.ReadKey();
+            Console.WriteLine(pos.ToPosition());
+            Console.ReadLine(); 
         }
     }
 }
