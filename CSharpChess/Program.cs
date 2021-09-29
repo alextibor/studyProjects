4f8cf8259d255e7b4e3790dfd3e159472676190f
alextibor <alex.tibor@hotmail.com>
2021-09-29 00:28:59 -0300

    reset

--- CSharpChess/Program.cs
+++ CSharpChess/Program.cs
@@ -1,43 +1,9 @@
-﻿using System;
-using CSharpChess.board;
-using CSharpChess.chess;
+using System.Xml.Schema;
 
 namespace CSharpChess
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
-        {
-            try
-            {
-                ChessMatch match = new ChessMatch();
-
-                while (!match.finished)
-                {
-                    Console.Clear();
-                    Screen.printBoard(match.bd);
-
-                    Console.WriteLine();
-                    Console.WriteLine("Origin: ");
-                    Position origin = Screen.readChessPosition().ToPosition();
-
-                    bool[,] possiblePositions = match.bd.piece(origin).possibleMoves();
-                    
-                    Console.Clear();
-                    Screen.printBoard(match.bd, possiblePositions);
-                    Console.WriteLine("Destino: ");
-                    Position destiny = Screen.readChessPosition().ToPosition();
-                    
-                    match.performMove(origin, destiny);
-                }
-                
-                Screen.printBoard(match.bd);
-            }
-            catch (BoardException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            Console.ReadLine();
-        }
+        
     }
 }
