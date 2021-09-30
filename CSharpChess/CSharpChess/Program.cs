2b67e3bd9b510910fb2343179d8292350f2522a7
AlexPcTrampo <alex.tibor@hotmail.com>
2021-09-29 22:19:26 -0300

    156

--- CSharpChess/CSharpChess/Program.cs
+++ CSharpChess/CSharpChess/Program.cs
@@ -11,12 +11,22 @@ namespace CSharpChess
     {
         static void Main(string[] args)
         {
-            Board bd = new Board(8, 8);
+            try
+            {
+                Board bd = new Board(8, 8);
             
-            bd.PutPiece(new Tower(bd, Color.Black), new Position(0, 0));
-            bd.PutPiece(new Tower(bd, Color.Black), new Position(1, 3));
-            bd.PutPiece(new King(bd, Color.Black), new Position(2, 4));
-            Screen.PrintBoard(bd);
+                bd.PutPiece(new Tower(bd, Color.Black), new Position(0, 0));
+                bd.PutPiece(new Tower(bd, Color.Black), new Position(1, 3));
+                bd.PutPiece(new King(bd, Color.Black), new Position(5, 0));
+                Screen.PrintBoard(bd);
+                Console.ReadLine();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
             Console.ReadLine();
         }
     }
--- CSharpChess/CSharpChess/board/Board.cs
+++ CSharpChess/CSharpChess/board/Board.cs
@@ -18,11 +18,44 @@ namespace CSharpChess.board
             return pieces[line, column];
         }
 
+        public Piece ReturnBoardPiece(Position pos)
+        {
+            return pieces[pos.line, pos.column];
+        }
+
+        public bool ThereIsAPiece(Position pos)
+        {
+            ValidatePosition(pos);
+            return ReturnBoardPiece(pos) != null;
+        }
+
         public void PutPiece(Piece p, Position pos)
         {
+            if (ThereIsAPiece(pos))
+            {
+                throw new BoardException("There is already a piece in this position");
+            }
             pieces[pos.line, pos.column] = p;
             p.position = pos;
         }
+
+        public bool ValidPosition(Position pos)
+        {
+            if (pos.line < 0 || pos.line >= lines || pos.column < 0 || pos.column >= columns)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void ValidatePosition(Position pos)
+        {
+            if (!ValidPosition(pos))
+            {
+                throw new BoardException("Invalid Position!");
+            }
+        }
+        
         
     }
 }
