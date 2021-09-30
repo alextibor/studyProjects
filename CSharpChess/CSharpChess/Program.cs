c7d6b25183fa727ccd14b5928cd46863b4a482fc
AlexPcTrampo <alex.tibor@hotmail.com>
2021-09-29 22:30:27 -0300

    157

--- CSharpChess/CSharpChess/Program.cs
+++ CSharpChess/CSharpChess/Program.cs
@@ -11,22 +11,8 @@ namespace CSharpChess
     {
         static void Main(string[] args)
         {
-            try
-            {
-                Board bd = new Board(8, 8);
-            
-                bd.PutPiece(new Tower(bd, Color.Black), new Position(0, 0));
-                bd.PutPiece(new Tower(bd, Color.Black), new Position(1, 3));
-                bd.PutPiece(new King(bd, Color.Black), new Position(5, 0));
-                Screen.PrintBoard(bd);
-                Console.ReadLine();
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
-
+            ChessPosition cp = new ChessPosition('a', 1);
+            Console.WriteLine(cp.toPosition());
             Console.ReadLine();
         }
     }
