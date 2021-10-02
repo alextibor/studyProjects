cb866aeb811b8b9873d06727b741cceb8e8d5729
Tibor <alex.tibor@hotmail.com>
2021-10-02 00:22:57 -0300

    implemented board class

---
+++ ChessJavaConsole/src/com/chess/Main.java
@@ -0,0 +1,12 @@
+package com.chess;
+
+import com.chess.boardgame.Position;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Position pos = new Position(8, 1);
+        System.out.println(pos);
+    }
+}
