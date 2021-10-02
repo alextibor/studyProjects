e832ef39187d0b19d0048a4e2bf8b1f3bbef0b0d
Tibor <alex.tibor@hotmail.com>
2021-10-02 02:31:46 -0300

    Implementing something

--- ChessJavaConsole/src/com/console/chess/ChessMatch.java
+++ ChessJavaConsole/src/com/console/chess/ChessMatch.java
@@ -1,8 +1,6 @@
 package com.console.chess;
 
 import com.console.boardgame.Board;
-import com.console.boardgame.Position;
-import com.console.chess.pieces.King;
 import com.console.chess.pieces.Rook;
 
 public class ChessMatch{
@@ -24,9 +22,16 @@ public class ChessMatch{
         return mat;
     }
 
+    private void placeNewPiece(char column, int row, ChessPiece piece){
+        board.placePiece(piece, new ChessPosition(column,row).toPosition());
+    }
+
     private void initialSetup() {
-        board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-        board.placePiece(new King(board, Color.BLACK), new Position(2, 1));
-        board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+
+        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 8, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+        //placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+        //placeNewPiece('b', 6, new Rook(board, Color.WHITE));
     }
 }
