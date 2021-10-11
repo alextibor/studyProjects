73e0ee944b6676b38dbb20c8a365ee0997207289
AlexTibor <alex.tibor@hotmail.com>
2021-10-11 17:17:22 -0300

    Alexander

--- bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
+++ bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferecia',
@@ -6,7 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent {
+
+  @Output() aoTranferir = new EventEmitter<any>();
+
+  valor: number = 0;
+  destino: number = 0;
+
   transferir() {
     console.log('Solicitada nova transferecia');
+    const valorEmitir = {valor: this.valor, destino: this.destino}
+    this.aoTranferir.emit(valorEmitir);
   }
 }
