2ad0b05f3e4d60e59568bbbe61019b522e86d048
alexDesktop <alex.tibor@hotmail.com>
2021-10-18 11:03:15 -0300

    Clearing Screen

--- ChessJavaConsole/src/com/console/Main.java
+++ ChessJavaConsole/src/com/console/Main.java
@@ -1,6 +1,7 @@
 package com.console;
 
 import com.console.boardgame.Position;
+import com.console.chess.ChessException;
 import com.console.chess.ChessMatch;
 import com.console.chess.ChessPiece;
 import com.console.chess.ChessPosition;
@@ -10,20 +11,27 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
 
         while(true){
-            UI.printBoard(chessMatch.getPieces());
-            System.out.println();
-            System.out.println("Source: ");
-            ChessPosition source = UI.readChessPosition(sc);
+            try{
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.println("Source: ");
+                ChessPosition source = UI.readChessPosition(sc);
 
-            System.out.println();
-            System.out.println("Target: ");
-            ChessPosition target = UI.readChessPosition(sc);
+                System.out.println();
+                System.out.println("Target: ");
+                ChessPosition target = UI.readChessPosition(sc);
 
-            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            } catch (ChessException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
         }
 
     }
