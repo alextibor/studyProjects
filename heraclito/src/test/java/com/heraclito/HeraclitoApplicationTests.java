a7166f867605463a6e7558c9a94ce6e5350a8c73
alexDesktop <alex.tibor@hotmail.com>
2021-10-18 19:31:17 -0300

    started heraclito

---
+++ heraclito/src/test/java/com/heraclito/HeraclitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heraclito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeraclitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
