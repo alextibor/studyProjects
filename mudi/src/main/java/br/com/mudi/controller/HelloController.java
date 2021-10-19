48414997e0abf54c4e0a01fe454d46c2b6e0fd78
alexDesktop <alex.tibor@hotmail.com>
2021-10-19 02:03:37 -0300

    started implementing bootstrap

--- mudi/src/main/java/br/com/mudi/controller/HelloController.java
+++ mudi/src/main/java/br/com/mudi/controller/HelloController.java
@@ -1,7 +1,12 @@
 package br.com.mudi.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HelloController {
 
+    @GetMapping("/hello")
     public String hello(){
         return "hello";
     }
