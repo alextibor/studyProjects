56dc48267cea507ac2643ca337aec6e3559f28c5
alexDesktop <alex.tibor@hotmail.com>
2021-10-19 14:50:02 -0300

    starting springTranslater

--- mudi/src/main/java/br/com/mudi/controller/HomeController.java
+++ mudi/src/main/java/br/com/mudi/controller/HomeController.java
@@ -1,27 +1,26 @@
 package br.com.mudi.controller;
 
 import br.com.mudi.controller.model.Pedido;
+import br.com.mudi.repository.PedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private PedidoRepository repository;
+
     @GetMapping("/home")
     public String home(Model model){
-
-        Pedido pedido = new Pedido();
-        pedido.setNomeProduto("Echo Dot (3ª Geração)");
-        pedido.setUrlImagem("https://m.media-amazon.com/images/I/61mjTR81OtL._AC_SL1000_.jpg");
-        pedido.setUrlProduto("https://www.amazon.com.br/dp/B07PDHSJ1H/?pf_rd_r=J1HHY305Y7MSYFWVSRY6&pf_rd_p=71f03b49-1a00-4f1c-a43b-fb4d5cd4f6ae&pd_rd_r=e4720818-acd2-4429-9cf6-9d52e05ea31d&pd_rd_w=dEFDx&pd_rd_wg=3H6n2&ref_=pd_gw_unk");
-        pedido.setDescricao("O Echo Dot é o nosso smart speaker de maior sucesso.");
-
-        List<Pedido> pedidos = Arrays.asList(pedido);
-
+        List<Pedido> pedidos = repository.recuperaTodosOsPedidos();
         model.addAttribute("pedidos", pedidos);
         return "home";
     }
---
+++ springTranslater/src/main/java/com/springTranslater/SpringTranslaterApplication.java
@@ -0,0 +1,13 @@
+package com.springTranslater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTranslaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTranslaterApplication.class, args);
+	}
+
+}
