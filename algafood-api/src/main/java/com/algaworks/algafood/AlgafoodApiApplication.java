5675d4a95cff799db6e6976b12369199b2479223
alexDesktop <alex.tibor@hotmail.com>
2021-10-22 22:27:21 -0300

    add maven dependency

--- algafood-api/src/main/java/com/algaworks/algafood/AlgafoodApiApplication.java
+++ algafood-api/src/main/java/com/algaworks/algafood/AlgafoodApiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AlgafoodApiApplication {
+public class AlgafoodApiApp lication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlgafoodApiApplication.class, args);
