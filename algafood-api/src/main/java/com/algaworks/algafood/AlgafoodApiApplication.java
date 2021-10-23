2f627e3fec0c63b064315ae1673bc5725b50b549
alexDesktop <alex.tibor@hotmail.com>
2021-10-23 03:06:14 -0300

    att

--- algafood-api/src/main/java/com/algaworks/algafood/AlgafoodApiApplication.java
+++ algafood-api/src/main/java/com/algaworks/algafood/AlgafoodApiApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AlgafoodApiApp lication {
-
+public class AlgafoodApiApplication {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AlgafoodApiApplication.class, args);
 	}
