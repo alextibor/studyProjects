3a6594d1cccb3a9bd41401fa493864423409f32a
alexDesktop <alex.tibor@hotmail.com>
2021-11-09 09:54:49 -0300

    add alexESR again

---
+++ alexFood/src/main/java/com/alexFood/AlexFoodApplication.java
@@ -0,0 +1,12 @@
+package com.alexFood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlexFoodApplication.class, args);
+	}
+}
