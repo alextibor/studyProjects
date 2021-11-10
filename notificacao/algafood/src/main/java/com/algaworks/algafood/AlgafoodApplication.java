ceba314f7caacaabfa1861c3e85102fa27f4abef
alexDesktop <alex.tibor@hotmail.com>
2021-11-10 17:13:07 -0300

    finalizando aula injeção de dependencias

--- notificacao/algafood/src/main/java/com/algaworks/algafood/AlgafoodApplication.java
+++ notificacao/algafood/src/main/java/com/algaworks/algafood/AlgafoodApplication.java
@@ -6,6 +6,9 @@ import com.algaworks.algafood.model.*;
 import com.algaworks.algafood.notificador.*;
 import com.algaworks.algafood.service.*;
 
+
+// 2.19. Desambiguação de beans com anotação customizada.mp4
+// PAREI NESSA AULA
 @SpringBootApplication
 public class AlgafoodApplication {
 
