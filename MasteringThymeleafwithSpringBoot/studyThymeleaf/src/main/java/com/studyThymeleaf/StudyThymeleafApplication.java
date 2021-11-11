0c6e870c5a2f8f9446ad09b9ed95652fa1d2dbd4
alexDesktop <alex.tibor@hotmail.com>
2021-11-11 19:03:32 -0300

    setting up project

--- MasteringThymeleafwithSpringBoot/studyThymeleaf/src/main/java/com/studyThymeleaf/StudyThymeleafApplication.java
+++ MasteringThymeleafwithSpringBoot/studyThymeleaf/src/main/java/com/studyThymeleaf/StudyThymeleafApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StudyThymeleafApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(StudyThymeleafApplication.class, args);
 	}
 
