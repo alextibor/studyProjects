92a4af8fbc6d151992e73a92aa485c3466c09baa
alexDesktop <alex.tibor@hotmail.com>
2021-11-16 18:09:01 -0300

    stockConsultant start

---
+++ stockConsultant/src/main/java/com/stockConsultant/StockConsultantApplication.java
@@ -0,0 +1,13 @@
+package com.stockConsultant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockConsultantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockConsultantApplication.class, args);
+	}
+
+}
