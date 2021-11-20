f9e1976e2972d7953a22bfd19228b7f7d897eda9
alexDesktop <alex.tibor@hotmail.com>
2021-11-20 04:47:52 -0300

    created echo bot

--- niceStockBot/src/main/java/com/niceStockBot/Main.java
+++ niceStockBot/src/main/java/com/niceStockBot/Main.java
@@ -1,7 +1,21 @@
 package com.niceStockBot;
 
-public class Main {
-    public static void main(String[] args) {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+public class Main {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new MyAmazingBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
--- niceStockBot/src/main/java/com/niceStockBot/MyAmazingBot.java
+++ niceStockBot/src/main/java/com/niceStockBot/MyAmazingBot.java
@@ -5,7 +5,7 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-public class MyAwesomeBot extends TelegramLongPollingBot{
+public class MyAmazingBot extends TelegramLongPollingBot{
 
     @Override
     public String getBotUsername(){
