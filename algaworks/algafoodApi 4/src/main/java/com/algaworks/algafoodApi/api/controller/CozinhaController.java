88aa06ceaa210634ce2c9d864ab5c131525f7d3f
alexDesktop <alex.tibor@hotmail.com>
2021-11-21 02:40:51 -0300

    update

---
+++ algaworks/algafoodApi 4/src/main/java/com/algaworks/algafoodApi/api/controller/CozinhaController.java
@@ -0,0 +1,26 @@
+package com.algaworks.algafoodApi.api.controller;
+
+import com.algaworks.algafoodApi.domain.model.Cozinha;
+import com.algaworks.algafoodApi.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @GetMapping
+    public List<Cozinha> listar() {
+        return cozinhaRepository.listar();
+    }
+}
