57fc2f9e1e8f69b701160666701f524beffdd690
AlexDesktop <alex.tibor@hotmail.com>
2021-11-25 23:23:32 -0300

    JacksonXML

--- algaworks/algafoodApi 4/src/main/java/com/algaworks/algafoodApi/api/controller/CozinhaController.java
+++ algaworks/algafoodApi 4/src/main/java/com/algaworks/algafoodApi/api/controller/CozinhaController.java
@@ -4,13 +4,9 @@ import com.algaworks.algafoodApi.domain.model.Cozinha;
 import com.algaworks.algafoodApi.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
 
 @RestController
 @RequestMapping("/cozinhas")
@@ -22,5 +18,10 @@ public class CozinhaController {
     @GetMapping
     public List<Cozinha> listar() {
         return cozinhaRepository.listar();
+    }
+
+    @GetMapping("{/cozinhaId}")
+    public Cozinha listar(@PathVariable("cozinhaId") Long id) {
+        return cozinhaRepository.buscar(id);
     }
 }
