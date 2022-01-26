031f444e83af8232c9936e9dae35c249ff15a367
alextibor <alex.tibor@hotmail.com>
2022-01-26 19:23:05 -0300

    c lesson

---
+++ C/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void lendoDados(){
+    char letra;
+    int nro;
+    float n;
+    double d;
+
+    scanf("%c",&letra);
+    scanf("%d",&nro);
+    scanf("%f",&n);
+    scanf("%f",&d);
+
+    printf("%c \n", letra);
+    printf("%d \n", nro);
+    printf("%f \n", n);
+    printf("%f \n", d);
+}
+
+void substituindoVariaveis(){
+
+    char letra = 'a';
+    int nro = 10;
+    float n = 5.25;
+    printf("%c \n", letra);
+    printf("%d \n", letra);
+    printf("%d \n", nro);
+    printf("%f \n", n);
+}
+
+void helloWorld(){
+    printf("Hello, World!\n");
+    printf("Hello There\n");
+    printf("Hello There");
+}
+
+int main() {
+
+    lendoDados();
+    return 0;
+}
