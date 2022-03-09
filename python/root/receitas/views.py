77966e610d086d449a890321bcb29713924bcf72
alex@desktop <alex.tibor@hotmail.com>
2022-03-08 22:07:43 -0300

    doneToday

--- python/root/receitas/views.py
+++ python/root/receitas/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
+
+def main(request):
+
+    receitas = {
+        1:'Lasanha',
+        2:'Sopa de Legumes',
+        3:'Sorvete',
+        4:'Bolo de chocolate',
+        5:'Bolo de cenoura',
+        6:'Ficasse de frango'
+    }
+
+    dados = {
+        'nome_das_receitas' : receitas
+    }
+    return render(request, 'main.html', dados)
+
+def cadastrar(request):
+    return render(request, 'cadastrar.html')
