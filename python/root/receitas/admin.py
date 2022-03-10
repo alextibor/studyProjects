c2c72fa2a6683540e6b0150cf45969ada637275b
alex@desktop <alex.tibor@hotmail.com>
2022-03-09 21:58:45 -0300

    doneToday3

--- python/root/receitas/admin.py
+++ python/root/receitas/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Receita
 
-# Register your models here.
+class ListandoReceitas(admin.ModelAdmin):
+    list_display = ('id', 'nome_receita', 'categoria', 'tempo_preparo')
+    list_display_links = ()
+
+admin.site.register(Receita)
--- python/root/receitas/urls.py
+++ python/root/receitas/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.index, name='index'),
+	path('index', views.index, name='index'),
 	path('main', views.main, name='main'),
 	path('cadastrar', views.cadastrar, name='cadastrar'),
+	path('<int:receita_id>', views.receita, name='receita')
 ]
