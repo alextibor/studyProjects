ce8357c62265d9fc64ee158794641ff023d76fcc
alex@desktop <alex.tibor@hotmail.com>
2022-05-24 18:01:59 -0300

    organizing

--- python/PythonAlura/Aula1/test2.py
+++
@@ -1,5 +0,0 @@
-from conta import Conta
-
-conta2 = Conta(789, "Alex", 89.0, 1675.0)
-
-print(conta2)
--- python/root/receitas/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-	path('index', views.index, name='index'),
-	path('main', views.main, name='main'),
-	path('cadastrar', views.cadastrar, name='cadastrar'),
-	path('<int:receita_id>', views.receita, name='receita')
-]
